b8279223f5da800a9c8e5846bfc711f82e0d5953
Anartz Mugika <mugan86@gmail.com>
2019-01-19 13:41:05 +0100

    File creator OK

---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const shell = require("shelljs");
+
+// Examples to use figlet font http://www.figlet.org/examples.html
+// inquirer documentation: https://www.npmjs.com/package/inquirer#examples
+const init = () => {
+  console.log(
+    chalk.blue(
+      figlet.textSync("File creator with CLI", {
+        font: "bubble",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  );
+};
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "What is the name of the file without extension?"
+    },
+    {
+      type: "list",
+      name: "EXTENSION",
+      message: "What is the file extension?",
+      choices: [".rb", ".js", ".php", ".css"],
+      filter: function(val) {
+        return val.split(".")[1];
+      }
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const createFile = (filename, extension) => {
+  const filePath = `${process.cwd()}/${filename}.${extension}`
+  shell.touch(filePath);
+  return filePath;
+};
+
+const success = filepath => {
+  console.log(
+    chalk.white.bgBlue.bold(`Well done! File created at ${filepath}`)
+  );
+};
+
+const run = async () => {
+  // show script introduction
+  init();
+
+  // ask questions
+  const answers = await askQuestions();
+
+  console.log(answers);
+  const { FILENAME, EXTENSION } = answers;
+
+  // create the file
+  const filePath = createFile(FILENAME, EXTENSION);
+
+  // show success message
+  success(filePath);
+};
+
+run();
